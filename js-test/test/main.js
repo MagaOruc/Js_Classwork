c98ef74d50c0fd752fd992e7bf8b1e35951ad406
məhəmməd <126715828+MagaOruc@users.noreply.github.com>
2023-04-12 13:52:06 +0400

    Add files via upload

---
+++ js-test/test/main.js
@@ -0,0 +1,125 @@
+//  let myName ="Mahammad"
+ 
+//  for (let i=0;i< myName.length;i++){
+// console.log(myName[i])
+
+// }
+
+// let m=0
+// for (; m<=8; m++)
+
+// {if (m%2==0)
+//     console.log(m)
+// }
+ 
+    
+
+
+
+
+
+
+
+
+
+
+
+// let k=0
+// for( ; k <=8; k++ ){
+//      if( k%2== 0 ){
+//         console.log( `${k} is even number`)
+        
+        
+//      }
+//      else{ console.log( `${k} is odd number`) }
+// }
+
+
+
+// const person = {
+//     firstName: "John",
+//     lastName : "Doe",
+//     id       : 5566,
+//     fullName : function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   };
+
+
+
+// const obj1={
+//     id:1,
+//     firstName: "Mahammad",
+//     lastName :"Orucov",
+//     location: "GDU"
+
+// }
+// console.log(obj1);
+
+
+// const obj2={
+//     id:1,
+//     firstName: "Mahammad",
+//     lastName :"Orucov",
+//     location: "GDU"
+
+// }
+// console.log(obj2);
+
+// const obj3={
+//     id:3,
+   
+
+// }
+// console.log(obj3);
+
+// const arr1=[0,1,2,3,4,5,6,7,8,9,10]
+// console.log(arr1);
+
+
+// const arr2=[77,false,"maga"]
+// console.log(arr2);
+
+
+// const arr31=[1,2,3,4,5]
+// const arr32=arr31
+// console.log(arr32);
+
+
+
+// const arr4= [0,1,2,3,4,5,6,7]
+// arr4.push(8,9)
+// console.log(arr4)
+
+
+
+// const arr5=[5,6,7]
+// arr5.shift(5)
+// arr5.unshift(1)
+
+// console.log(arr5)
+
+
+// const arr6=["Mahammad","Orucov", 45,"Quba"]
+// arr6[3]="Qusar"
+// console.log(arr6)
+
+
+
+
+// const arr7 =[123,345,456]
+// for(let item of arr7)
+// console.log(arr7)
+
+
+
+
+
+
+
+
+
+
+
+
+
