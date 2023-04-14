a2f464ff594448dda54f385a5df6ade779e5160d
məhəmməd <126715828+MagaOruc@users.noreply.github.com>
2023-04-14 14:20:44 +0400

    Add files via upload

---
+++ 14.04arrey_ method/main.js
@@ -0,0 +1,73 @@
+//  const arr1=['a','b','c','d','e']
+// // arr1.splice(1,1) //1
+// //arr1.splice(2,2)  //2
+// //arr1.splice( arr1.length-1)  //3
+// // arr1.splice( arr1.length-3)  //4
+//  // arr1.splice(3,0 ,'3','4')  //5
+//   arr1.splice(0,2,'A','B')
+
+// console.log(arr1);
+
+// const arr2=['user1','user2','user3',11,3,5]
+//  let newArr=arr2.slice();
+//   let newArr1=arr2.slice(1,5);
+//   let newArr2=arr2.slice(2);
+
+
+
+// console.log(newArr)
+// console.log(newArr1)
+// console.log(newArr2)
+
+//   3
+// const arr3=[1,2,3]
+// const arr4=[4,5,6]
+
+// let newArr33=arr3.concat(arr4)
+// console.log(newArr33);
+
+// console.log(arr3.concat(['a','b']));
+
+        //4  
+        const arr4=['name','surname',10,'age','city', 'uni',80,20]
+        // arr4.forEach(function(item, index, array) {
+        //     console.log(item);
+
+        // })
+
+        // arr4.forEach(item=>{
+        //   if( typeof item == 'number'){
+
+        //       console.log(item);
+        // }
+        // })
+
+        // 5 
+         const arr5=[4,5,6,'2',true, undefined, '55']
+          // const newArr=arr5.map((item) => (console.log(item)))
+
+        //   const newArr=arr5.map((item) =>{
+        //     if (typeof item == 'number'){
+        //         console.log(item *5)
+        //     }
+
+            
+        //   })
+          const newArr1=arr5.map((item) => console.log(String(item)))
+
+
+          const newArr2=arr5.map((item) => console.log(Boolean(item)))
+
+
+          const newArr3=arr5.map((item) => console.log(Number(item)))
+
+
+  
+
+
+
+
+
+
+
+
